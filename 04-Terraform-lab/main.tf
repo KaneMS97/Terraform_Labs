8c038a699b961d5dff882b3c8e2e0c8eeb6f70c3
Kane Stephens <kane_stephens@protonmail.com>
2026-05-21 11:04:40 +0100

    lab 4

--- 04-Terraform-lab/main.tf
+++ 04-Terraform-lab/main.tf
@@ -8,3 +8,90 @@ resource "aws_vpc" "main" {
     Enviroment = var.enviroment
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name       = "private-subnet"
+    Enviroment = var.enviroment
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name       = "terraform-course"
+    Enviroment = var.enviroment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name       = "private-subnet"
+    Enviroment = var.enviroment
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name       = "main-route-table"
+    Enviroment = var.enviroment
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.main.id
+
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Example secuirty group for our VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name       = "example-security-group"
+    Enviroment = var.enviroment
+  }
+}
+
